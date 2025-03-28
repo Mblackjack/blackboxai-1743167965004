b52b5698c86930ad19d965cb4c8a2efd8065e16f
Mblackjack <104918794+Mblackjack@users.noreply.github.com>
2025-03-28 10:19:43 -0300

    Add controllers/authController.js 1743167983394

---
+++ controllers/authController.js
@@ -0,0 +1,100 @@
+const jwt = require('jsonwebtoken');
+const User = require('../models/User');
+const catchAsync = require('../utils/catchAsync');
+const AppError = require('../utils/appError');
+
+const signToken = id => {
+  return jwt.sign({ id }, process.env.JWT_SECRET, {
+    expiresIn: process.env.JWT_EXPIRE
+  });
+};
+
+const createSendToken = (user, statusCode, res) => {
+  const token = signToken(user._id);
+  
+  res.status(statusCode).json({
+    status: 'success',
+    token,
+    data: {
+      user
+    }
+  });
+};
+
+exports.register = catchAsync(async (req, res, next) => {
+  const { name, email, password, role, phone, companyName, cnpj, serviceCategories } = req.body;
+
+  // Check if email already exists
+  const existingUser = await User.findOne({ email });
+  if (existingUser) {
+    return next(new AppError('Email already in use', 400));
+  }
+
+  const newUser = await User.create({
+    name,
+    email,
+    password,
+    role,
+    ...(role === 'client' && { 
+      phone,
+      profileImage: req.file ? req.file.path : undefined 
+    }),
+    ...(role === 'provider' && { 
+      companyName, 
+      cnpj,
+      serviceCategories,
+      profileImage: req.file ? req.file.path : undefined
+    })
+  });
+
+  // Remove password from output
+  newUser.password = undefined;
+
+  createSendToken(newUser, 201, res);
+});
+
+exports.login = catchAsync(async (req, res, next) => {
+  const { email, password } = req.body;
+
+  if (!email || !password) {
+    return next(new AppError('Please provide email and password', 400));
+  }
+
+  const user = await User.findOne({ email }).select('+password');
+
+  if (!user || !(await user.correctPassword(password, user.password))) {
+    return next(new AppError('Incorrect email or password', 401));
+  }
+
+  createSendToken(user, 200, res);
+});
+
+exports.protect = catchAsync(async (req, res, next) => {
+  let token;
+  if (req.headers.authorization && req.headers.authorization.startsWith('Bearer')) {
+    token = req.headers.authorization.split(' ')[1];
+  }
+
+  if (!token) {
+    return next(new AppError('You are not logged in! Please log in to get access.', 401));
+  }
+
+  const decoded = await jwt.verify(token, process.env.JWT_SECRET);
+  const currentUser = await User.findById(decoded.id);
+
+  if (!currentUser) {
+    return next(new AppError('The user belonging to this token no longer exists.', 401));
+  }
+
+  req.user = currentUser;
+  next();
+});
+
+exports.restrictTo = (...roles) => {
+  return (req, res, next) => {
+    if (!roles.includes(req.user.role)) {
+      return next(new AppError('You do not have permission to perform this action', 403));
+    }
+    next();
+  };
+};
